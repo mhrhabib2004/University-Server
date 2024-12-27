f24ab4d5f3572b41a0ac1fb718b7ee88484eb8cb
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-12-28 03:00:56 +0600

    up:student servicec

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -129,8 +129,8 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   return result;
 };
 
-const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.findOne({ id })
+const getSingleStudentFromDB = async (_id: string) => {
+  const result = await Student.findById({ _id })
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
@@ -179,7 +179,7 @@ const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
 
   console.log(modifiedUpdatedData);
 
-  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+  const result = await Student.findByIdAndUpdate( id , modifiedUpdatedData, {
     new: true,
     runValidators: true,
   });
@@ -192,8 +192,8 @@ const deleteStudentFromDB = async (id: string) => {
   try {
     session.startTransaction();
 
-    const deletedStudent = await Student.findOneAndUpdate(
-      { id },
+    const deletedStudent = await Student.findByIdAndUpdate(
+      id ,
       { isDeleted: true },
       { new: true, session },
     );
@@ -202,8 +202,11 @@ const deleteStudentFromDB = async (id: string) => {
       throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
     }
 
-    const deletedUser = await User.findOneAndUpdate(
-      { id },
+    // get user _id from deletedStudent
+    const userId = deletedStudent.user;
+
+    const deletedUser = await User.findByIdAndUpdate(
+      userId,
       { isDeleted: true },
       { new: true, session },
     );
