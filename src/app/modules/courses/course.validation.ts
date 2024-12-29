568e9bb327b640d4cbdc2adab9976f4ec49a4680
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-12-29 22:33:58 +0600

    add:course validation

---
+++ src/app/modules/courses/course.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const preRequistiteCourseSchema = z.object({
+    course:z.string(),
+    isDeletad:z.boolean().optional()
+})
+const createCourseValidationSchema = z.object({
+     body:z.object({
+        title:z.string(),
+        prefix:z.string(),
+        code:z.number(),
+        credits:z.number(),
+        preRequistiteCourses:z.array(preRequistiteCourseSchema)
+     })
+}) 
+
+export const courseavalidation = {
+    createCourseValidationSchema
+}
